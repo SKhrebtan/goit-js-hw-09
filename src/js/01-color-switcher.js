1de41dd48383e37f25128c6daee6150173994ce5
SKhrebtan <shrebtan@gmail.com>
2023-06-22 10:07:31 +0300

    1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const refs = {
+    startButton: document.querySelector('button[data-start]'),
+    stopButton: document.querySelector('button[data-stop]'),
+}
+let intervalId = null;
+let counter = 0;
+refs.startButton.addEventListener('click', onStartButton);
+refs.stopButton.addEventListener('click', onStopButton);
 
+function onStartButton() {
+    intervalId = setInterval(() => {
+        console.log(counter += 1)
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000)
+    refs.startButton.setAttribute('disabled', '');
+
+};
+
+function onStopButton(e) {
+    clearInterval(intervalId);
+    refs.startButton.removeAttribute('disabled');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
