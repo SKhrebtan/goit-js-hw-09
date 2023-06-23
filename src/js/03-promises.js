9d43e6f8a2a0bfd57476c6ccc5a1194304b4e3f6
SKhrebtan <shrebtan@gmail.com>
2023-06-23 16:35:10 +0300

    start task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,35 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
+const promiseGenerator = document.querySelector('.form')
+
+promiseGenerator.addEventListener('input', onFormSubmit)
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  const { delay, step, amount } = e.currentTarget;
+  let firstDelay = delay.value;
+  let delayStep = step.value;
+  let Amount = amount.value;
+
+  function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
+     resolve({ position, delay })
   } else {
-    // Reject
-  }
-}
+    reject({ position, delay })}}, delay)
+     
+  })
+  };
+  
+createPromise(firstDelay, delayStep)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+};
+
+
+
